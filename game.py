a074a81447186b1a5cf03e2b757924907c6a5c99
Inwydix <99067158+Inwydix@users.noreply.github.com>
2022-12-16 17:41:21 +0100

    Add files via upload

---
+++ game.py
@@ -0,0 +1,29 @@
+verb1 = input("Enter a verb: ")
+verb2 = input("Enter another verb: ")
+verb3 = input("Enter another verb: ")
+word1 = input("Enter a noun: ")
+word2= input("Enter another noun: ")
+word3 = input("Enter another noun: ")
+word4 = input("Enter another noun: ")
+word5 = input("Enter another noun: ")
+word6 = input("Enter another noun: ")
+word7 = input("Enter another noun: ")
+plural = input("Enter a plural noun: ")
+name = input("Enter a feminine name: ")
+color = input("Enter a color: ")
+phrase = input("Enter a phrase: ")
+time = input("Enter a period of time: ")
+food = input("Enter a food: ")
+
+print("Say what you will, or say what you " + verb1 +
+      "\nThere’s no " + word1 + " who is like " + name +
+      "\nTo all whom she passes, she gives a “" + phrase + "”"
+      "\nAnd she’s ready with " + word2 + ", if you’ve been led astray."
+      "\nHer " + word3 + " is " + color + " with two streaks of gray."
+      "\nAnd her purse may be made out of " + word4 + " and " + word5 +
+      "\nBut her " + plural + " have been known to go on for " + time +
+      "\nAnd she wakes up each morning with something to " + verb2 + "."
+      "\nShe had her " + word6 + " built in a tree by the bay,"
+      "\nAnd dangles a " + word7 + " swing down every May."
+      "\nShe’s famous for making " + food +
+      "\nShe’ll give you five boxes, but won’t let you " + verb3)
